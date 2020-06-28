8882a4a70c3f98c0a419f9ee3f4a6d3ba88fe4f4
Dawid Dahl <dawiddahl@gmail.com>
2020-06-28 17:49:20 +0200

    testing2

--- src/client/sagas/socketGenerators.ts
+++ src/client/sagas/socketGenerators.ts
@@ -12,7 +12,7 @@ import {
 } from "../actions/socketActions";
 import socketService from "../utils/socketService";
 
-const SERVER_URL = process.env.SERVER_URL && "";
+const SERVER_URL = "";
 
 export function* subscribe(socket: SocketIOClient.Socket) {
 	const emittedEvent = yield* _select(getEmittedEvent);
@@ -61,7 +61,7 @@ export function* workerSocketListen(socket: SocketIOClient.Socket) {
 export function* workerSocketConnect() {
 	const socket: SocketIOClient.Socket | null = yield call(
 		[socketService, socketService.connect],
-		"/"
+		SERVER_URL!
 	);
 
 	if (!socket) throw new Error("Socket connection couldn't be established.");
